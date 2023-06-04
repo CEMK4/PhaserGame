f610464472e7b1f963ed2d1934d8b07939254e33
cemkAHAHA <ceme4k4@mail.ru>
2023-06-04 19:40:39 +0300

    Done

--- game.js
+++ game.js
@@ -1,98 +1,130 @@
-class Example extends Phaser.Scene {
-    movingPlatform;
-    cursors;
-    platforms;
-    stars;
-    player;
-
+class Example extends Phaser.Scene
+{
+ 	
     addPlayer(playerInfo) {
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
         this.physics.add.collider(this.player, this.platforms);
         this.physics.add.collider(this.player, this.movingPlatform);
-        this.physics.add.collider(this.stars, this.platforms);
-        this.physics.add.collider(this.stars, this.movingPlatform);
     }
 
     addOtherPlayers(playerInfo) {
-        var player = this.physics.add.sprite(100, 450, 'dude');
-        player.setBounce(0.2);
-        player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, this.platforms);
-        this.physics.add.collider(this.player, this.movingPlatform);
-        this.physics.add.collider(this.stars, this.platforms);
-        this.physics.add.collider(this.stars, this.movingPlatform);
-        player.id = playerInfo.id;
-        this.otherPlayers.add(player);
+	var otherPlayer = this.physics.add.sprite(100, 450, 'dude');
+        otherPlayer.setBounce(0.2);
+        otherPlayer.setCollideWorldBounds(true);
+        this.physics.add.collider(otherPlayer, this.platforms);
+        this.physics.add.collider(otherPlayer, this.movingPlatform);
+        otherPlayer.id = playerInfo.id;
+        this.otherPlayers.add(otherPlayer);
     }
-
-    playerMoved(data) {
-        console.log(this.otherPlayers[data.id]);
-        if (this.otherPlayers[data.id]) {
-            this.otherPlayers[data.id].setPosition(data.x, data.y);
-        }
-    }
-
-    preload() {
-        this.load.image('sky', 'assets/sky.png');
+    
+    preload ()
+    {        
+        this.load.image('sky', 'assets/sky.png');        
         this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('star', 'assets/star.png');        
+        this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
     }
 
-    create() {
+    create ()
+    {        
         this.add.image(400, 300, 'sky');
-
+        
         this.platforms = this.physics.add.staticGroup();
-
         this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
+        
         this.movingPlatform = this.physics.add.image(400, 400, 'ground');
-
         this.movingPlatform.setImmovable(true);
         this.movingPlatform.body.allowGravity = false;
-        this.movingPlatform.setVelocityX(50);
-
-        this.player = this.physics.add.sprite(100, 450, 'dude');
-
-        var self = this;
+        this.movingPlatform.setVelocityX(50);          
+        
+        this.player = this.physics.add.sprite(100, 450, 'dude');        
+	this.otherPlayers = this.add.group();
+	
+	this.stars = this.physics.add.group();	
+	
         this.socket = io();
-        this.otherPlayers = this.physics.add.group();
-
-        this.socket.on('currentPlayers', function (players) {
-          Object.keys(players).forEach(function (id) {
-            if (players[id].playerId === self.socket.id) {
-              addPlayer(self, players[id]);
-            } else {
-              addOtherPlayers(self, players[id]);
-            }
-          });
-        });
-
-        this.socket.on('newPlayer', function (playerInfo) {
-          addOtherPlayers(self, playerInfo);
-        });
-
-        this.socket.on('disconnect', function (playerId) {
-          self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerId === otherPlayer.playerId) {
-              otherPlayer.destroy();
-            }
-          });
-        });
-
+        const self = this;
+	
+	this.socket.on("currentPlayers", function(players) {
+	      Object.keys(players).forEach(function(id) {
+	      
+	        console.log(self.socket.id);
+		if (players[id].id === self.socket.id) {
+		  self.addPlayer(players[id]);
+		} else {
+		  self.addOtherPlayers(players[id]);
+		}
+	      });
+	    });
+        
+        this.socket.on("newPlayer", function(playerInfo) {
+	      self.addOtherPlayers(playerInfo);
+	    });
+        
+        
+        this.socket.on('playerDisconnect', function (playerId) {
+            console.log(self.otherPlayers);
+	    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+	      if (playerId === otherPlayer.id) {
+		otherPlayer.destroy();
+	      }
+	    });
+	  });
+        
         this.socket.on('playerMoved', function (data) {
-            self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-                if (data.player.id === otherPlayer.id) {
-                    if (data.anim === 'turn')
-                        otherPlayer.anims.play(data.anim)
-                    else otherPlayer.anims.play(data.anim, true)
-                    otherPlayer.setPosition(data.player.x, data.player.y);
-                }
-            });
+	  self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+	    if (data.player.id === otherPlayer.id) {
+          if (data.anim === 'turn')
+            otherPlayer.anims.play(data.anim)
+          else otherPlayer.anims.play(data.anim, true)
+	      otherPlayer.setPosition(data.player.x, data.player.y);
+	    }
+	  });
+	});
+        
+        this.socket.on('placeStars', function(stars) {
+	  Object.values(stars).forEach(function(star) {
+	    var new_star = self.physics.add.sprite(star.x, star.y, 'star');
+	    new_star.id = star.id;
+	    self.stars.add(new_star);	    
+	    new_star.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));	       	    
+	  }); 
+	  self.physics.add.collider(self.stars, self.platforms);
+	  self.physics.add.collider(self.stars, self.movingPlatform);
+	});
+		       
+        this.socket.on('starsMovement', function(starsData) {          
+	  starsData.forEach(function(starData) {
+	    self.stars.getChildren().forEach(function(star) {
+	      if (star.id === starData.id) {
+		star.setPosition(starData.x, starData.y);
+		star.setVelocityX(starData.velocityX);
+	      }
+	    });
+	  });
+	});
+	
+	this.socket.on('platformMovement', function(platformData) {          
+	  self.movingPlatform.setPosition(platformData.x, platformData.y);	        
+	});
+              
+       
+        this.socket.on('collectStar', function(id) {
+          self.stars.getChildren().forEach(function (star) {
+          	if (star.id === id)          	
+          	    star.disableBody(true, true);          	
+          });          
+	});
+	
+	
+	this.physics.add.overlap(this.player, this.stars, (player, star) =>
+        {            
+            star.disableBody(true, true);
+            this.socket.emit('collectStar', star.id);
         });
-
-        this.anims.create({
+		
+	this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
             frameRate: 10,
@@ -101,7 +133,7 @@ class Example extends Phaser.Scene {
 
         this.anims.create({
             key: 'turn',
-            frames: [{ key: 'dude', frame: 4 }],
+            frames: [ { key: 'dude', frame: 4 } ],
             frameRate: 20
         });
 
@@ -110,57 +142,86 @@ class Example extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
-        });
-
+        });	
+		
         this.cursors = this.input.keyboard.createCursorKeys();
+    }  
 
-        this.stars = this.physics.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
-        });
-
-        for (const star of this.stars.getChildren()) {
-            star.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        }       
-
-        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
-    }
-
-    update() {
+    update ()
+    {
         const { left, right, up } = this.cursors;
-
-        if (left.isDown) {
+        var anim = '';        
+        
+        if (left.isDown)
+        {
             this.player.setVelocityX(-160);
-
-            this.player.anims.play('left', true);
+            anim = 'left'
+            this.player.anims.play(anim, true);
         }
-        else if (right.isDown) {
+        else if (right.isDown)
+        {
             this.player.setVelocityX(160);
-
-            this.player.anims.play('right', true);
+            anim = 'right'
+            this.player.anims.play(anim, true);
         }
-        else {
+        else
+        {
             this.player.setVelocityX(0);
-
-            this.player.anims.play('turn');
+            anim = 'turn'
+            this.player.anims.play(anim);
         }
 
-        if (up.isDown && this.player.body.touching.down) {
+        if (up.isDown && this.player.body.touching.down)
+        {
             this.player.setVelocityY(-330);
         }
-
+        
         if (this.movingPlatform.x >= 500) {
-            this.movingPlatform.setVelocityX(-50);
-        }
-        else if (this.movingPlatform.x <= 300) {
-            this.movingPlatform.setVelocityX(50);
-        }
+	    this.movingPlatform.setVelocityX(-50);
+	}
+	else if (this.movingPlatform.x <= 300) {
+	    this.movingPlatform.setVelocityX(50);
+	}
+        
+        var x = this.player.x;
+	var y = this.player.y;	
+	if (this.player.oldPosition && (x !== this.player.oldPosition.x || y !== this.player.oldPosition.y)) {
+	  this.socket.emit('playerMovement', { x: this.player.x, y: this.player.y, anim: anim});
+	}
+	 	
+	this.player.oldPosition = {
+	  x: this.player.x,
+	  y: this.player.y	  
+	};
+	
+        var starsData = this.getStarsData(this.stars.getChildren());
+        this.socket.emit('starsMovement', starsData);
+        
+        var platformData = this.getPlatformData(this.movingPlatform);
+        this.socket.emit('platformMovement', platformData);
     }
 
-    collectStar(player, star) {
-        star.disableBody(true, true);
-    }
+   getStarsData(stars) {
+	  const starsData = [];
+	  stars.forEach(star => {
+	    const starData = {
+	      x: star.x,
+	      y: star.y,
+	      id: star.id,
+	      velocityX: star.body.velocity.x
+	    };
+	    starsData.push(starData);
+	  });
+	  return starsData;
+	}
+	
+   getPlatformData(platform){
+   	const platformData = {
+   		x: platform.x, 
+   		y: platform.y  		  		
+   	};
+   	return platformData;
+   }
 }
 
 const config = {
@@ -171,8 +232,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
-            debug: false
+            debug: false,
+            gravity: { y: 300 }
         }
     },
     scene: Example
--- server.js
+++ server.js
@@ -1,38 +1,69 @@
 var express = require('express')
 app = express()
-var server = require('http').createServer(app)
+server = require('http').createServer(app)
 var io = require('socket.io')(server)
-
 app.use(express.static(__dirname))
-
-server.listen(80)
-console.log('Server has started')
+server.listen(8081)
+console.log('started')
 
 var players = {};
+var stars = {};
+var host = null; 
 
 io.on('connection', function (socket) {
-    console.log('Подключился пользователь ' + socket.id);
-
-    var player = {
-        id: socket.id,
+    console.log('User ' + socket.id + ' connected' );
+    if (!host) {
+    	host = socket.id;
+    	for (var i = 0; i < 11; i++) {
+	  var x = 12 + 70*i;
+	  var y = 0;
+	  stars[i] = { x: x, y: y, id: i};
+	}	
+    }
+    socket.emit('placeStars', stars);
+  
+    players[socket.id] = {        
         x: 0,
-        y: 0
-    };
-    players[socket.id] = player;
-
+        y: 0,
+        id: socket.id
+    };    
+        
+    socket.emit('currentPlayers', players);    
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+    
     socket.on('playerMovement', function (data) {
 	  players[socket.id].x = data.x;
 	  players[socket.id].y = data.y;	  
 	  socket.broadcast.emit('playerMoved', { player: players[socket.id], anim: data.anim });
 	});
-
-    socket.on('disconnect', function () {
-        console.log('Пользователь '  + socket.id + ' отключился');
-        delete players[socket.id];        
-        socket.broadcast.emit('playerDisconnect', socket.id);
-    });
     
-    socket.emit('currentPlayers', players);    
-    socket.broadcast.emit('newPlayer', player);
+    socket.on('starsMovement', function (stars) {
+    	if (host === socket.id) {
+	  socket.broadcast.emit('starsMovement', stars);
+	}});
+	
+    socket.on('platformMovement', function (platform) {
+    	if (host === socket.id) {
+	  socket.broadcast.emit('platformMovement', platform);
+	}});
+
+    socket.on('collectStar', function (id) {        
+  	delete stars[id]
+        socket.broadcast.emit('collectStar', id);
+    });
+        
+    socket.on('disconnect', function () {
+        console.log('User ' + socket.id + ' disconnected');
+        delete players[socket.id];   
+             
+        socket.broadcast.emit('disconnectPlayer', socket.id);        
+            if (socket.id === host) {
+            host = null;
+            const newHost = players[Object.keys(players)[0]];
+            if (newHost) {
+              host = newHost.id;
+            }
+        }        
+    });  
 });
 
